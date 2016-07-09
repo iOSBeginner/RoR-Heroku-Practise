2c523bb681d355749abce27ce3de8d486a21e834
蘇健豪 <jenhausu@icloud.com>
2016-07-09 19:41:02 +0800

    add new respond when user's messages are not consider.

--- mainController.rb
+++ mainController.rb
@@ -11,6 +11,8 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Welcome to http://sitepoint.com")
     when '/topics'
         bot.api.send_message(chat_id: message.chat.id, text: "topics text")
+    else
+        bot.api.send_message(chat_id: message.chat.id, text: "I don't understand what you say....")
     end
   end
 end
