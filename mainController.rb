8e78aeb32d71bcebacbbbdceac8f22d194a95df5
蘇健豪 <jenhausu@icloud.com>
2016-07-09 18:48:07 +0800

    fix bug

--- mainController.rb
+++ mainController.rb
@@ -4,9 +4,10 @@ token = '240556961:AAEP-A47vhju8Vy3P7C7vZZTdGseOpdmY9I'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
+    when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/sitepoint'
       bot.api.send_message(chat_id: message.chat.id, text: "Welcome to http://sitepoint.com")
-    end
     when '/topics'
         bot.api.send_message(chat_id: message.chat.id, text: "topics text")
     end
